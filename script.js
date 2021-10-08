9bbfd949f094309ac29319648d763703bff2e0e9
Hernan Di Tano <hditano@gmail.com>
2021-10-08 00:46:33 -0300

    finished flex challenges

--- script.js
+++ script.js
@@ -44,3 +44,55 @@ getPhotos.addEventListener('click', () => {
         })
     });
 })
+
+
+const myUsers = document.querySelector('.my-users');
+const usersBtn = document.querySelector('.get-users');
+let myArray = [];
+
+async function getUsers() {
+    const response = await fetch('users.json');
+    const users = await response.json();
+    myArray = users;
+    // getDisplay();
+    return users
+}
+
+// function getDisplay() {
+//     myArray.forEach((ele) => {
+//         myUsers.innerHTML +=  `<p>User is ${ele.username}</p>`
+//     })
+// }
+
+getUsers().then(user => {
+    user.map(ele => {
+        myUsers.innerHTML += `<p>User is ${ele.username}</p>`
+    })
+})
+
+getUsers();
+
+/* My Details */
+
+const myDetails = document.querySelector('.my-details');
+const DETAILS_URL = 'https://jsonplaceholder.typicode.com/users/3';
+const getDetailsButton = document.querySelector('.get-details');
+
+
+async function getDetails() {
+    const response = await fetch(DETAILS_URL);
+    const resolve = await response.json();
+    return resolve;
+}
+
+getDetailsButton.addEventListener('click', () => {
+    getDetails().then(ele => {
+        myDetails.innerHTML = `<p>Name: ${ele.name}</p>
+                               <p>Username: ${ele.username}</p>
+                               <p>Company: ${ele.company.name}</p>
+                               <p>Details: </p>
+                               <p>Email:${ele.email}</p>
+                               <p>Phone: ${ele.phone}</p>
+                               <p>Website: ${ele.website}</p>  `
+    })
+})
